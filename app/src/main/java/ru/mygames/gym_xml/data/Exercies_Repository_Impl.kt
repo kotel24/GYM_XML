56ec0d66c64421bee14bec1839f8f9c1159f313e
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-02-24 22:24:47 +0300

    прописал в дата классе реализацию для use case

--- app/src/main/java/ru/mygames/gym_xml/data/Exercies_Repository_Impl.kt
+++ app/src/main/java/ru/mygames/gym_xml/data/Exercies_Repository_Impl.kt
@@ -1,5 +1,43 @@
 package ru.mygames.gym_xml.data
 
-object Exercies_Repository_Impl {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import ru.mygames.gym_xml.domain.Exercies
+import ru.mygames.gym_xml.domain.Exercies_Repository
 
+object Exercies_Repository_Impl: Exercies_Repository {
+
+    private var exerciesList = mutableListOf<Exercies>()
+
+    private var mutableListLiveData = MutableLiveData<List<Exercies>>()
+
+    private var autoIncrementId = 0
+    override fun addExercies(exercies: Exercies) {
+        if (exercies.id == Exercies.UNDEFINED_ID)
+            exercies.id = autoIncrementId++
+        exerciesList.add(exercies)
+        updateExercies()
+    }
+
+    override fun deleteExercies(exercies: Exercies) {
+        exerciesList.remove(exercies)
+        updateExercies()
+    }
+
+    override fun editExercies(exercies: Exercies) {
+        deleteExercies(getExerciesById(exercies.id))
+        addExercies(exercies)
+    }
+
+    override fun getExerciesById(id: Int): Exercies {
+        return exerciesList.find { it.id == id }?: throw RuntimeException("Element with id $id not found")
+    }
+
+    override fun getExercies(): LiveData<List<Exercies>> {
+        return mutableListLiveData
+    }
+
+    private fun updateExercies(){
+        mutableListLiveData.postValue(exerciesList.toList())
+    }
 }
