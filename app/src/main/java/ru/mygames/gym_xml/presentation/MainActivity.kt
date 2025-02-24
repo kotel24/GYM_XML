dffca8b27f1e0668c4ed00c4c2e4af27b2ce6304
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-02-24 21:52:58 +0300

    перенё main activity in presentation

--- app/src/main/java/ru/mygames/gym_xml/presentation/MainActivity.kt
+++ app/src/main/java/ru/mygames/gym_xml/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package ru.mygames.gym_xml
+package ru.mygames.gym_xml.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ru.mygames.gym_xml.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
