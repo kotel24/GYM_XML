1e622c7bc74a501fa6f23604caa9b4151dd4a75a
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-02-24 22:23:57 +0300

    присвоил начальное значение id = UNDEFINED_ID

--- app/src/main/java/ru/mygames/gym_xml/domain/Exercies.kt
+++ app/src/main/java/ru/mygames/gym_xml/domain/Exercies.kt
@@ -1,9 +1,13 @@
 package ru.mygames.gym_xml.domain
 
 data class Exercies(
-    val id: Int,
     val name: String,
     val description: String,
     val durationInSeconds: String,
     val videoUrl: String,
-)
+    var id: Int = UNDEFINED_ID,
+){
+    companion object{
+        const val UNDEFINED_ID = -1
+    }
+}
