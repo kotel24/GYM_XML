69d4743c54e84b73f8593b362b76331e371009df
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-03-10 21:55:41 +0300

    изменил маин вью модел

--- app/src/main/java/ru/mygames/gym_xml/presentation/MainViewModel.kt
+++ app/src/main/java/ru/mygames/gym_xml/presentation/MainViewModel.kt
@@ -1,4 +1,26 @@
 package ru.mygames.gym_xml.presentation
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+import ru.mygames.gym_xml.data.Exercies_Repository_Impl
+import ru.mygames.gym_xml.domain.Delete_Exercies_Use_Case
+import ru.mygames.gym_xml.domain.Edit_Exercies_Use_Case
+import ru.mygames.gym_xml.domain.Exercies
+import ru.mygames.gym_xml.domain.Get_Exercies_Use_Case
+
+class MainViewModel:ViewModel() {
+    private val repository = Exercies_Repository_Impl
+    private val getExercisesUseCase = Get_Exercies_Use_Case(repository)
+    private val deleteExercises = Delete_Exercies_Use_Case(repository)
+    private val editExercises = Edit_Exercies_Use_Case(repository)
+
+    val exercieslist = getExercisesUseCase.getExercises()
+
+    fun deleteExercises(exercies: Exercies) {
+        deleteExercises.deleteExercies(exercies)
+    }
+
+    fun editExercises(exercies: Exercies) {
+        val newItem = exercies.copy(enabled = !exercies.enabled)
+        editExercises.editExercies(newItem)
+    }
 }
