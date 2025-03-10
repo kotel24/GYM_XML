1156ab1f7dd650d4b5f866a9507d07db7253c57d
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-03-10 16:52:00 +0300

    у упражнений будет кол-во повторений или продолжительность по времени

--- app/src/main/java/ru/mygames/gym_xml/domain/Exercies.kt
+++ app/src/main/java/ru/mygames/gym_xml/domain/Exercies.kt
@@ -3,8 +3,9 @@ package ru.mygames.gym_xml.domain
 data class Exercies(
     val name: String,
     val description: String,
-    val durationInSeconds: String,
+    val durationOrCount: String,
     val videoUrl: String,
+    val enabled: Boolean,
     var id: Int = UNDEFINED_ID,
 ){
     companion object{
