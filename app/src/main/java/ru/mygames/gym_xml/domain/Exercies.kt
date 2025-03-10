62e666671090d12963ed0be8f3308a8218bd66cd
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-03-10 21:02:58 +0300

    пока изменил параметры Exercies

--- app/src/main/java/ru/mygames/gym_xml/domain/Exercies.kt
+++ app/src/main/java/ru/mygames/gym_xml/domain/Exercies.kt
@@ -2,9 +2,7 @@ package ru.mygames.gym_xml.domain
 
 data class Exercies(
     val name: String,
-    val description: String,
     val durationOrCount: String,
-    val videoUrl: String,
     val enabled: Boolean,
     var id: Int = UNDEFINED_ID,
 ){
