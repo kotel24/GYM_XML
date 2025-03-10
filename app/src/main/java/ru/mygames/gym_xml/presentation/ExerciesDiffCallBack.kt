d94f47210a20696688e24ae9a22406a00c41b193
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-03-10 21:09:08 +0300

    написал класс для оптимизации работы адаптера

---
+++ app/src/main/java/ru/mygames/gym_xml/presentation/ExerciesDiffCallBack.kt
@@ -0,0 +1,14 @@
+package ru.mygames.gym_xml.presentation
+
+import androidx.recyclerview.widget.DiffUtil
+import ru.mygames.gym_xml.domain.Exercies
+
+class ExerciesDiffCallBack:DiffUtil.ItemCallback<Exercies> (){
+    override fun areItemsTheSame(oldItem: Exercies, newItem: Exercies): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Exercies, newItem: Exercies): Boolean {
+        return oldItem == newItem
+    }
+}
